13a18795a0fa9207934f6860d8a3178acbe20ebb
Jason Fox <jasonrobertfox@gmail.com>
2013-12-11 11:54:03 -0300

    I fixed the bug in the algorithm that was not optimizing the second players score, really the "second player" is just a different player and durring its turn wants to maximize the score, so the x or the o is somewhat a non issue.

--- lib/app/player/perfect_player.rb
+++ lib/app/player/perfect_player.rb
@@ -9,12 +9,12 @@ module App
         @game_state = game_state
         @player = @game_state.active_turn
         @other_payer = @player == 'x' ? 'o' : 'x'
-        @choice = [];
+        @choice = []
 
         if @game_state.get_blanks.length == 9
           # If its the first move pick a random good starting move
           puts 'start-move'
-          @game_state.get_new_state([[0,2].sample,[0,2].sample])
+          @game_state.get_new_state([[0, 2].sample, [0, 2].sample])
         elsif @game_state.get_blanks.length == 1
           # Pick the last available space if there is only one
           puts 'last-move'
@@ -22,20 +22,17 @@ module App
         else
           # Determine the best possible move via a minimax calculation
           puts 'ai-move'
-          if @game_state.active_turn == 'x'
-            max_choice(@game_state, @choice)
-          else
-            min_choice(@game_state, @choice)
-          end
+          # Whoever is playing is trying to maximize their game
+          max_choice(@game_state)
           @game_state.get_new_state(@choice)
         end
       end
 
       private
 
-        def max_choice(state, choice)
+        def max_choice(state)
           # Check for an end condition
-          n = -100000
+          n = -100_000
           if state.win?(@player)
             return 1
           elsif state.win?(@other_payer)
@@ -46,9 +43,7 @@ module App
             # Loop through each available space
             state.get_blanks.each do | choice |
               new_state = state.get_new_state(choice)
-              next_choice = min_choice(new_state, [])
-              # next_choice = next_choice.is_a?(Integer) ? { choice: choice, score: next_choice } : next_choice
-              # best_choice = next_choice if next_choice[:score] > best_choice[:score]
+              next_choice = min_choice(new_state)
               if next_choice > n
                 n = next_choice
                 @choice = choice
@@ -58,8 +53,8 @@ module App
           end
         end
 
-        def min_choice(state, choice)
-          n = 100000
+        def min_choice(state)
+          n = 100_000
           if state.win?(@player)
             return 1
           elsif state.win?(@other_payer)
@@ -70,13 +65,11 @@ module App
             # Loop through each available space
             state.get_blanks.each do | choice |
               new_state = state.get_new_state(choice)
-              next_choice = max_choice(new_state, [])
+              next_choice = max_choice(new_state)
               if next_choice < n
                 n = next_choice
                 @choice = choice
               end
-              # next_choice = next_choice.is_a?(Integer) ? { choice: choice, score: next_choice } : next_choice
-              # best_choice = next_choice if next_choice[:score] < best_choice[:score]
             end
             return n
           end
