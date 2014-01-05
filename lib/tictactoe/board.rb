c6a784ea3150d737f04aa2a507f8c8958d286237
Jason Fox <jasonrobertfox@gmail.com>
2014-01-05 20:55:11 -0300

    Partly implement enhanced board class.

---
+++ lib/tictactoe/board.rb
@@ -0,0 +1,82 @@
+# Encoding: utf-8
+
+module Tictactoe
+  class Board
+    BLANK = ''
+
+    attr_reader :size, :available_moves, :corner_spaces, :winner
+
+    def initialize(size)
+      @size = size
+      @moves_left = size**2
+      @moves_made = 0
+      @board = Array.new(size) { Array.new(size, '') }
+      @available_moves = (0..size - 1).to_a.product((0..size - 1).to_a)
+      @corner_spaces = [0, size - 1].product([0, size - 1])
+    end
+
+    def place_piece(piece, row, column)
+      @board[row][column] = piece
+      @available_moves.delete([row, column])
+      @has_pieces ||= true
+      @moves_left -= 1
+      @moves_made += 1
+
+      # check for win if a win is possible
+      if @moves_made >= 2 * size - 1
+        check_for_win
+      end
+    end
+
+    def piece_at(row, column)
+      @board[row][column]
+    end
+
+    def blank?
+      ! @has_pieces
+    end
+
+    def last_move?
+      @moves_left == 1
+    end
+
+    def over?
+      @winner || draw?
+    end
+
+    def draw?
+      @moves_left == 0 && !@winner
+    end
+
+    def check_for_win
+      @winner = winning_row || winning_column
+    end
+
+    def winning_row
+      @board.each do |row|
+        candidate = row[0]
+        unless candidate == BLANK
+          (1..size - 1).to_a.each do |column|
+            break unless row[column] == candidate
+            return candidate
+          end
+        end
+      end
+      nil
+    end
+
+    def winning_column
+      (0..size - 1).to_a.each do |column|
+        candidate = @board[0][column]
+        unless candidate == BLANK
+          (1..size - 1).to_a.each do |row|
+            break unless @board[row][column] == candidate
+            return candidate
+          end
+        end
+      end
+      nil
+    end
+
+  end
+end
---
+++ spec/unit/lib/tictactoe/board_spec.rb
@@ -0,0 +1,81 @@
+# Encoding: utf-8
+
+require 'spec_helper'
+require 'tictactoe/board'
+
+describe Tictactoe::Board do
+
+  let(:board) { Tictactoe::Board.new(3) }
+
+  it 'should be initialized with it size' do
+    board.size.should eq 3
+  end
+
+  it 'should let a piece be set' do
+    board.place_piece('x', 0, 2)
+    board.piece_at(0, 2).should eq 'x'
+  end
+
+  it 'should return a full array of available moves' do
+    board.available_moves.should eq [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]]
+    board.blank?.should be_true
+  end
+
+  it 'should return an array of corner spaces' do
+    board.corner_spaces.should eq [[0, 0], [0, 2], [2, 0], [2, 2]]
+  end
+
+  it 'should not be blank if a piece has been placed on it' do
+    board.place_piece('x', 0, 2)
+    board.blank?.should be_false
+  end
+
+  it 'should not show a move that has been made' do
+    board.place_piece('x', 0, 0)
+    board.available_moves.should eq [[0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]]
+  end
+
+  it 'should report if there is only a single move left' do
+    b = build_board('xoxoxox__')
+    b.last_move?.should be_false
+    b.place_piece('x', 2, 1)
+    b.last_move?.should be_true
+  end
+
+  it 'should report win information for a row victory' do
+    %w(xxxo_o___ o__xxx_o_ o__o__xxx).each do |code|
+      b = build_board code
+      b.winner.should eq('x'), "Failed with #{code}"
+      b.draw?.should be_false
+      b.over?.should be_true
+    end
+  end
+
+  it 'should report win information for a column victory' do
+    %w(x_oxo_x__ _xo_x_ox_ __x_ox_ox).each do |code|
+      b = build_board code
+      b.winner.should eq('x'), "Failed with #{code}"
+      b.draw?.should be_false
+      b.over?.should be_true
+    end
+  end
+
+
+
+end
+
+def build_board(code)
+  board = Tictactoe::Board.new(3)
+  row = 0
+  column = 0
+  code.split(//).each do |c|
+    board.place_piece(c, row, column) unless c == '_'
+    if column == 2
+      column = 0
+      row += 1
+    else
+      column += 1
+    end
+  end
+  board
+end
