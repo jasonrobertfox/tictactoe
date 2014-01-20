c2004e97a4be67336ca33b3defabff00d58c599c
Jason Fox <jasonrobertfox@gmail.com>
2014-01-20 17:22:26 -0300

    Add board dependency to game-state.

--- lib/tictactoe/game_state.rb
+++ lib/tictactoe/game_state.rb
@@ -1,32 +1,66 @@
 # Encoding: utf-8
 
+require 'tictactoe/board'
+
 module Tictactoe
   class GameState
     BLANK = ''
 
-    attr_reader :available_moves, :corner_spaces, :number_of_spaces, :player_piece, :opponent_piece, :board, :winner
+    attr_reader :player_piece, :opponent_piece, :winner
 
     def initialize(size, player_piece, opponent_piece)
       @player_piece, @opponent_piece, @size = player_piece, opponent_piece, size
-      @board = Array.new(@size) { Array.new(@size, BLANK) }
+      # @board = Array.new(@size) { Array.new(@size, BLANK) }
+      @board = Board.new(size)
       initialize_board_meta_data
     end
 
     def place_piece(piece, coordinate)
-      unless piece == BLANK
-        @board[coordinate.first][coordinate.last] = piece
-        update_moves(coordinate)
+      @board.place_piece(piece, coordinate)
+      # unless piece == BLANK
+      #   @board[coordinate.first][coordinate.last] = piece
+      #   update_moves(coordinate)
         check_for_win if win_possible?
-      end
+      # end
       self
     end
 
+    # Refactor methods
+
+    def contents_of(x, y)
+      @board.contents_of([x, y])
+    end
+
+    def available_moves
+      @board.blank_spaces
+    end
+
+    def corner_spaces
+      @board.corner_spaces
+    end
+
+    def number_of_spaces
+      @board.number_of_spaces
+    end
+
+    def board
+      array = []
+      0.upto(@size - 1) do |r|
+        row = []
+        0.upto(@size - 1) do |c|
+          row << contents_of(r, c)
+        end
+        array << row
+      end
+      array
+    end
+
     def blank?
-      ! @has_pieces
+      @board.blank?
     end
 
     def last_move?
-      @moves_left == 1
+      @board.number_of_blanks == 1
     end
 
     def over?
@@ -34,7 +68,7 @@ module Tictactoe
     end
 
     def draw?
-      @moves_left == 0 && !@winner
+      @board.number_of_blanks == 0 && !@winner
     end
 
     def winner_exists?
@@ -57,8 +91,7 @@ module Tictactoe
 
     def hand_off
       copy = dup
-      copy.board = @board.map { |i| i.dup }
-      copy.available_moves = @available_moves.map { |i| i.dup }
+      copy.board = @board.dup
       copy.player_piece = @opponent_piece
       copy.opponent_piece = @player_piece
       copy
@@ -89,18 +122,18 @@ module Tictactoe
     end
 
     def win_possible?
-      @moves_made >= @minimum_moves_required_to_win
+      @board.number_of_occupied >= @minimum_moves_required_to_win
     end
 
     def check_for_win
-      @winner = winning_row(@board) || winning_column || winning_diagonal || winning_reverse_diagonal
+      @winner = winning_row || winning_column || winning_diagonal || winning_reverse_diagonal
     end
 
-    def winning_row(board)
-      board.each_with_index do |row, i|
-        candidate = row[0]
-        if candidate != BLANK && row.count(candidate) == @size
-          @winning_line = [i].product(@board_range) if @gather_line
+    def winning_row
+      0.upto(@size - 1) do |r|
+        candidate = contents_of(r, 0)
+        if candidate != BLANK && 0.upto(@size - 1).map { |c| contents_of(r, c) }.count(candidate) == @size
+          @winning_line = [r].product(@board_range) if @gather_line
           return candidate
         end
       end
@@ -108,26 +141,29 @@ module Tictactoe
     end
 
     def winning_column
-      result = winning_row(@board.transpose)
-      if result && @gather_line
-        @winning_line.map! { |i| i.reverse } if @gather_line
+      0.upto(@size - 1) do |c|
+        candidate = contents_of(0, c)
+        if candidate != BLANK && 0.upto(@size - 1).map { |r| contents_of(r, c) }.count(candidate) == @size
+          @winning_line = @board_range.product([c]) if @gather_line
+          return candidate
+        end
       end
-      result
+      nil
     end
 
     def winning_diagonal
-      check_diagonal(@board[0][0]) { |i| i }
+      check_diagonal(contents_of(0, 0)) { |i| i }
     end
 
     def winning_reverse_diagonal
-      check_diagonal(@board[@max_index][0]) { |i| @max_index - i }
+      check_diagonal(contents_of(@max_index, 0)) { |i| @max_index - i }
     end
 
     def check_diagonal(candidate)
       diagonal = []
       (0..@max_index).each do |i|
         column = yield i
-        return nil unless candidate != BLANK && @board[i][column] == candidate
+        return nil unless candidate != BLANK && contents_of(i, column) == candidate
         diagonal << [i, column] if @gather_line
       end
       @winning_line = diagonal if diagonal.count == @size
