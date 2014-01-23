5ce63dc1090344dee2e7dc6f036d79067b9ebd44
Jason Fox <jasonrobertfox@gmail.com>
2014-01-23 13:07:39 -0300

    Refactor board methods.

--- lib/tictactoe/board.rb
+++ lib/tictactoe/board.rb
@@ -4,16 +4,15 @@ module Tictactoe
   class Board
     BLANK = ''
 
-    attr_reader :width, :number_of_spaces, :number_of_blanks, :number_of_occupied, :corner_spaces
+    attr_reader :width, :number_of_spaces, :number_of_blanks, :number_of_occupied, :corner_spaces, :blank_spaces
 
     def initialize(width)
       @width = width
       @number_of_spaces = @number_of_blanks = width**2
       @number_of_occupied = 0
-      board_range = (0..(width - 1)).to_a
-      @blank_spaces = board_range.product(board_range).map { |s| s.join }
+      @blank_spaces = square_array(width.times.to_a)
       @board = Array.new(@number_of_spaces) { BLANK }
-      @corner_spaces = [0, width - 1].product([0, width - 1])
+      @corner_spaces = square_array([0, width - 1])
     end
 
     def blank?
@@ -22,34 +21,38 @@ module Tictactoe
 
     def place_piece(piece, space)
       unless piece == BLANK
-        i = space.first * @width + space.last
-        @board[i] = piece
+        @board[calculate_board_index(space)] = piece
         @number_of_blanks -= 1
         @number_of_occupied += 1
-        @blank_spaces.delete(space.join)
+        @blank_spaces.delete(space)
       end
       self
     end
 
     def contents_of(space)
-      i = space.first * @width + space.last
-      @board[i]
+      @board[calculate_board_index(space)]
     end
 
-    def blank_spaces
-      @blank_spaces.map { |space| space.split(//).map(&:to_i) }
+    def to_a
+      @board
     end
+
+    private
 
     def initialize_copy(source)
-      blank_spaces = @blank_spaces.dup
+      blank_spaces = @blank_spaces.map(&:dup)
       board = @board.dup
       super
       @blank_spaces = blank_spaces
       @board = board
     end
 
-    def to_a
-      @board
+    def square_array(array)
+      array.product(array)
+    end
+
+    def calculate_board_index(space)
+      space.first * @width + space.last
     end
   end
 end
